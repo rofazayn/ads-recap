81aba5deddb036ae1287e1ed9cbfb578264276b7
Abderraouf Zine <rofazayn@gmail.com>
2021-07-05 11:08:24 +0100

    Update linearSearch with pseudocode.

--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,19 @@
+/*
+  - Write a function that accepts and array and a value
+  - Loop through the array and check if the current array element is equal to the value
+  - If it is, returh the index at which the element is found
+  - If the value is never found, return -1
+*/
+
 function linearSearch(arr, val) {
+  // loop through array and check
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === val) {
+      // return the index if val found in arr
       return i;
     }
   }
+  // return -1 if val is never found
   return -1;
 }
 
