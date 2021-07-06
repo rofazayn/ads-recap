52fcde064b47f7a887ea5d015b52dad59311186e
Abderraouf Zine <rofazayn@gmail.com>
2021-07-06 12:40:08 +0100

    Create a template file for algorithms.

---
+++ template.js
@@ -0,0 +1,17 @@
+/*
+  Function name
+  - Condition 1.
+  - Condition 2.
+
+  Pseudocode:
+  - Step 1.
+  - Step 2.
+*/
+
+function functionName() {}
+
+// best case scenario: O()
+// worst case scenario: O()
+// average case scenario: O()
+
+console.log(functionName(/*args*/));
